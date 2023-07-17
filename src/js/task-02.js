20e0d51cb2a3551ccdf22ff90251af3df5e7b79a
Roman <jrj80mail@gmail.com>
2023-07-17 20:22:47 +0300

    2_3

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,31 @@
+//* Напиши скрипт, який для кожного елемента масиву 'ingredients':
+//*     Створить окремий елемент '<li>', використовуючи метод 'document.createElement()'
+//*     Додасть назву інгредієнта як його текстовий вміст.
+//*     Додасть елементу клас 'item'
+//*     Після чого, вставить усі <li> за одну операцію у список 'ul#ingredients'
+
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ul = document.getElementById("ingredients");
+//!ALTERNATIVE  const ul = document.querySelector("#ingredients");
+//? Фрагмент (DocumentFragment) - тимчасовий контейнер - використовується для збереження елементів до їх додавання в DOM.
+//* Він не прив'язаний безпосередньо до DOM-структури, тому додавання елементів до фрагменту не має впливу на відображення на сторінці
+const fragment = document.createDocumentFragment();
+
+ingredients.forEach((i) => {
+  const li = document.createElement("li"); //? 'createElement()'
+  li.textContent = i;
+  li.classList.add("item"); //? +class
+  // console.log(li);
+  fragment.appendChild(li); //? 'appendChild()' - метод для додавання одного елемента до іншого, 'append()' є методом для додавання елементів до DOM
+  // console.log(fragment);
+});
+
+ul.append(fragment);
--- src/js/task-03.js
+++ src/js/task-03.js
@@ -1,14 +1,43 @@
+//* Скрипт для створення галереї зображень на підставі масиву даних.
+//*          HTML містить список 'ul.gallery' < ul class="gallery" ></ >
+//* - Використовуй масив об'єктів 'images' для створення елементів <img>, вкладених в <li>.
+//?       Для створення розмітки використовуй шаблонні рядки і метод 'insertAdjacentHTML()'
+//* - Усі елементи галереї повинні додаватися в DOM за одну операцію додавання
+//* - Додай мінімальне оформлення галереї флексбоксами та грідами через CSS класи
+
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+
+//* Отримання посилання на елемент галереї
+// const ul = document.querySelector(".gallery"); //!MAIN
+// const ul = document.getElementsByClassName("gallery"); //! повертає HTMLCollection, який не має методу 'insertAdjacentHTML'
+const ul = document.getElementsByClassName("gallery")[0]; //* отримати перший елемент з HTMLCollection, щоб виправити цю попередню помилку
+// console.log(ul);
+
+//* Створення розмітки для кожного зображення і додати його до галереї
+//!------------------------------------------------------------------------
+//*  деструктуризація об'єкту (в параметрах map) - одразу отримуємо значення 'url' і 'alt' з кожного об'єкту масиву 'images'
+const galleryImg = images
+  .map(({ url, alt }) => `<li><img src="${url}" alt="${alt}" /></li>`)
+  .join(""); //*       insertAdjacentHTML потребує String
+//!ALTERNATIVE-------------------------------------------------------------
+// const galleryImg = images
+//   .map((image) => {return `<li><img src="${image.url}" alt="${image.alt}"></li>`;})
+//
+//   .join("");
+//!------------------------------------------------------------------------
+
+//* додаємо HTML-подібну розмітку в DOM
+ul.insertAdjacentHTML("beforeend", galleryImg);
