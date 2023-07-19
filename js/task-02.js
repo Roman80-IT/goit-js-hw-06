a497c6f2cf04e37f79855c96cb4d8f9d3ecd7af3
Roman <jrj80mail@gmail.com>
2023-07-19 22:12:22 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -15,27 +15,46 @@ const ingredients = [
 
 const ul = document.getElementById("ingredients");
 //!ALTERNATIVE  const ul = document.querySelector("#ingredients");
+
+//!------------------------      ВАРІАНТ 1    ( 'fragment'    forEach )     --------------------------------
 //? Фрагмент (DocumentFragment) - тимчасовий контейнер - використовується для збереження елементів до їх додавання в DOM.
 //* Він не прив'язаний безпосередньо до DOM-структури, тому додавання елементів до фрагменту не має впливу на відображення на сторінці
-const fragment = document.createDocumentFragment();
+// const fragment = document.createDocumentFragment();
 
-ingredients.forEach((i) => {
-  const li = document.createElement("li"); //? 'createElement()'
-  li.textContent = i;
-  li.classList.add("item"); //? +class
-  // console.log(li);
-  fragment.appendChild(li); //? 'appendChild()' - метод для додавання одного елемента до іншого, 'append()' є методом для додавання елементів до DOM
-  // console.log(fragment);
+// ingredients.forEach((i) => {
+//   const li = document.createElement("li"); //? 'createElement()'
+//   li.textContent = i;
+//   li.classList.add("item"); //? +class
+//   // console.log(li);
+//   fragment.appendChild(li); //? 'appendChild()' - метод для додавання одного елемента до іншого, 'append()' є методом для додавання елементів до DOM
+//   // console.log(fragment);
+// });
+
+// ul.append(fragment);
+
+//!--------------------     ВАРІАНТ 2    ( 'for'    оператор [...spread] )     -----------------------------
+// const lis = [];
+// for (let i = 0; i < ingredients.length; i++) {
+//   const item = document.createElement("li");
+//   item.textContent = ingredients[i];
+//   item.classList.add("item");
+//   lis.push(item);
+// }
+
+// ul.append(...lis);
+
+//!--------------------     ВАРІАНТ 3    ( 'map'    =>    оператор [...spread] )     -----------------------------
+const lis = ingredients.map((ingredient) => {
+  const li = document.createElement("li");
+  li.textContent = ingredient;
+  li.classList.add("item");
+  return li;
 });
 
-ul.append(fragment);
+ul.append(...lis);
 
-const listEl = document.querySelector("#ingredients");
-const itemsEl = [];
-for (let i = 0; i < ingredients.length; i++) {
-  const item = document.createElement("li");
-  item.textContent = ingredients[i];
-  item.classList.add("item");
-  itemsEl.push(item);
-}
-listEl.append(...itemsEl);
+//* За допомогою методу append() без оператора 'spread', потрібно створити кожен елемент <li> окремо, встановити їх текстовий вміст та клас,
+//* і потім додати кожен з них до списку < ul > за допомогою окремого виклику append() для кожного елемента.
+
+//*  використання оператора ...spread дозволяє передавати елементи масиву, як окремі аргументи методу append(),
+//* що забезпечує додавання декількох елементів до DOM.
